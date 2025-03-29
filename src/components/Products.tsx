aaf554be451e2b34fb8a8d9bb947af78c7fcda6e
mohamedsabrie <abosaadmohamed@gmail.com>
2025-03-29 13:12:31 +0700

    style(Products): adjust image container styling for consistent aspect ratio
    The changes ensure the image container maintains a consistent aspect ratio of 3:4 and uses absolute positioning for better responsiveness. This improves the visual consistency and layout of product images.

--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -8,14 +8,13 @@ function Products({ productsData }: { productsData: ProductType[] }) {
     <div className="grid  sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-x-3 gap-y-[30px] mt-24">
       {productsData.map((product) => (
         <div key={product.id} className="flex flex-col ">
-          <Link prefetch={false} href={`/product/${product.id}`} className="flex-1 flex justify-center items-center ">
+          <Link prefetch={false} href={`/product/${product.id}`} className="flex-1 flex justify-center items-center relative aspect-[3/4] w-full">
             <Image
               width={300}
               height={400}
               src={product.image}
               alt={product.title}
-              className="object-contain w-auto h-auto max-h-[400px]" 
-
+              className="object-contain absolute inset-0 w-full h-full"
             />
           </Link>
 
